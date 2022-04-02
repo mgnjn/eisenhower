385ee9bf54d00ddaf4116e8bd240c84edbc99578
ann <mamtbal@gmail.com>
2022-04-02 00:16:45 -0400

    Read from endpoint

--- front/README.md
+++
@@ -1,5 +0,0 @@
-## Overview
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-
--- front/src/components/TodoList.js
+++ front/src/components/TodoList.js
@@ -1,33 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
 import Todo from "./Todo";
+import axios from "axios";
 
 function TodoList({ todos, setTodos }) {
+  const [tempLoaded, setTempLoaded] = useState(false);
+  const [temp, setTemp] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await axios(`http://localhost:5000/todos[0]`);
+      let result = await response.data;
+      setTemp(result);
+      setTempLoaded(true);
+    }
+    fetchData();
+  }, []);
+
   const completeTodo = (id) => {
     const completedTodos = todos.filter(
       (todo) => todo.id !== id && todo.completed === true
     );
   };
+
   const removeTodo = (id) => {
     const newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(Object.assign([], newTodos));
   };
+
   return (
     <>
-      {
-        <ul>
-          {todos.map((todo) => {
-            return (
-              <Todo
-                key={todo.id}
-                id={todo.id}
-                todo={todo}
-                removeTodo={removeTodo}
-                completeTodo={completeTodo}
-              />
-            );
-          })}
-        </ul>
-      }
+      <ul>
+        {todos.map((todo) => {
+          return (
+            <Todo
+              key={todo.id}
+              id={todo.id}
+              todo={todo}
+              removeTodo={removeTodo}
+              completeTodo={completeTodo}
+            />
+          );
+        })}
+      </ul>
     </>
   );
 }
