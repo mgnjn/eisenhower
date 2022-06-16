3c454a38bd3beee1dfeba714fe5acc8513a32c0e
mgnjn <41850019+mgnjn@users.noreply.github.com>
2022-06-15 22:18:39 -0400

    Cleanup

--- front/src/components/Todo.js
+++ front/src/components/Todo.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import Checkbox from "@mui/material/Checkbox";
-
 import Button from "@mui/material/Button";
 import DeleteIcon from "@mui/icons-material/Delete";
+
 function Todo({ todo, removeTodo, completeTodo }) {
   const handleCheckboxClick = (id) => {
     completeTodo(id);
@@ -12,22 +12,24 @@ function Todo({ todo, removeTodo, completeTodo }) {
   };
   return (
     <>
-      <Checkbox
-        size="small"
-        onChange={() => {
-          handleCheckboxClick(todo.id);
-        }}
-      />
-      {todo.task}
-      <Button
-        size="small"
-        variant="text"
-        startIcon={<DeleteIcon />}
-        onClick={() => {
-          handleDeleteButtonClick(todo.id);
-        }}
-      />
-      <br></br>
+      <div>
+        <Checkbox
+          size="small"
+          onChange={() => {
+            handleCheckboxClick(todo.id);
+          }}
+        />
+        {todo.task}
+        <Button
+          size="small"
+          variant="text"
+          startIcon={<DeleteIcon />}
+          onClick={() => {
+            handleDeleteButtonClick(todo.id);
+          }}
+        />
+        <br></br>
+      </div>
     </>
   );
 }
