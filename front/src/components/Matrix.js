fc18bf0e0469a723fe8ed3f609f9712918bc01b7
mgnjn <41850019+mgnjn@users.noreply.github.com>
2022-06-21 19:48:55 -0400

    feat: make todo items draggable
    - React 18 doesn't work well with react-beautiful-dnd so to circumvent this issue, I just removed StrictMode.
    https://github.com/atlassian/react-beautiful-dnd/issues/2399#issuecomment-1137880007
    
    Not ideal though

--- front/src/components/Matrix.js
+++ front/src/components/Matrix.js
@@ -1,15 +1,18 @@
 import React from "react";
 import TodoBox from "./TodoBox";
 import Expiration from "./Expiration";
+import { DragDropContext } from "react-beautiful-dnd";
 
 function Matrix() {
   return (
     <>
-      <Expiration />
-      <TodoBox quadrant="q1" label="Important, Urgent" />
-      <TodoBox quadrant="q2" label="Important, Not Urgent" />
-      <TodoBox quadrant="q3" label="Not Important, Urgent" />
-      <TodoBox quadrant="q4" label="Not Important, Not Urgent" />
+      <DragDropContext>
+        <Expiration />
+        <TodoBox quadrant="q1" label="Important, Urgent" />
+        <TodoBox quadrant="q2" label="Important, Not Urgent" />
+        <TodoBox quadrant="q3" label="Not Important, Urgent" />
+        <TodoBox quadrant="q4" label="Not Important, Not Urgent" />
+      </DragDropContext>
     </>
   );
 }
