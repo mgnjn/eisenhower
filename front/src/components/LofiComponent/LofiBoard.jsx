f53a8a1e9404668db9486ce4e9e25020636ce060
mgnjn <41850019+mgnjn@users.noreply.github.com>
2022-07-07 04:38:09 -0400

    fix: no need to make divs hidden

--- front/src/components/LofiComponent/LofiBoard.jsx
+++ front/src/components/LofiComponent/LofiBoard.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { MODES } from "../../utils/constants";
 
 function LofiBoard({ modeStore }) {
-  return (
-    <div hidden={modeStore.activeMode !== MODES.lofiMode}>
-      LofiBoard {modeStore.activeMode}
-    </div>
-  );
+  return <div>LofiBoard {modeStore.activeMode}</div>;
 }
 
 export default LofiBoard;
--- front/src/components/StickyComponent/StickyBoard.jsx
+++ front/src/components/StickyComponent/StickyBoard.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { MODES } from "../../utils/constants";
 
 function StickyBoard({ modeStore }) {
-  return (
-    <div hidden={modeStore.activeMode !== MODES.stickyMode}>
-      StickyBoard {modeStore.activeMode}
-    </div>
-  );
+  return <div>StickyBoard {modeStore.activeMode}</div>;
 }
 
 export default StickyBoard;
